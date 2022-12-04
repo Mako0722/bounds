7165ad29e15aa1d929688379930e01f5ff65c0fa
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-04 23:16:41 +0900

    first commit

---
+++ bounds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bounds
+//
+//  Created by 中田誠 on 2022/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
