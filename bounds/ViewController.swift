8570df24e7c3a777811b1c9e204f161db5292172
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-04 23:33:51 +0900

    bounds

--- bounds/ViewController.swift
+++ bounds/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var arrow: UIImageView!
+    @IBOutlet weak var sky: UIView!
+    @IBOutlet weak var tombo: UIImageView!
+    
+    
+    @IBAction func catchTombo(_ sender: Any) {
+        var point = view.convert(tombo.center, from: sky)
+        point.x += arrow.bounds.width/2
+        point.y -= arrow.bounds.height/2
+        
+        arrow.center = point
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
